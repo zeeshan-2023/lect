fe4bb290278d99af73ffda65d410a429deee45d7
zeeshan abbas <zeeshan1905050100065@gmail.com>
2023-09-27 18:31:58 +0530

    edit

--- lect/settings.py
+++ lect/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'lect.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "DjangoCrudApp",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
